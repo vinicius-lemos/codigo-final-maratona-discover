2c94429641a2d5c66260983e395c332352d0b488
Vinicius Lemos <eeep.viniciuslemos@gmail.com>
2021-11-18 10:28:26 -0300

    DevFinaces com server

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const routes = express.Router()
-
-const basePath = __dirname + "/views/"
-
-routes.get("/", (req, res) => res.sendFile(basePath + "index.html"))
-
-module.exports = routes;
